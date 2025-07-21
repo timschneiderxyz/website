469555610853d156e4e404e90260ca49a04a732e
Tim Schneider <timschneider.xyz@gmail.com>
2025-07-21 11:15:34 +0200

    added biome

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = ({ site }) => new Response(`User-agent: *
+export const GET: APIRoute = ({ site }) => {
+  return new Response(`User-agent: *
 Allow: /
 
 Sitemap: ${new URL('sitemap-index.xml', site).href}`);
+};
